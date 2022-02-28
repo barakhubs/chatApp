b04f67605fb68bff2e318ebfa0ce0aac577cc762
mbbaraka <markbrightbaraka@gmail.com>
2022-02-27 23:03:12 -0800

    Working on the authentication and migrations

---
+++ resources/views/livewire/auth.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <div class="login-form">
+        <form>
+          <h1>Login</h1>
+          <div class="content">
+            <div class="input-field">
+              <input type="email" placeholder="Email" autocomplete="nope">
+            </div>
+            <div class="input-field">
+              <input type="password" placeholder="Password" autocomplete="new-password">
+            </div>
+            <a href="#" class="link">Forgot Your Password?</a>
+          </div>
+          <div class="action">
+            <button>Register</button>
+            <button>Sign in</button>
+          </div>
+        </form>
+      </div>
+</div>
